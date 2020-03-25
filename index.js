8b0be2061715fd2c0d494fe1d460b27983cac8d3
Stefanoren <renatus1812@163.com>
2020-03-25 15:18:43 +0800

    data api

--- index.js
+++ index.js
@@ -36,6 +36,66 @@ app.get('/tb_test', async (req, res) => {
     } catch (e) {
         res.send('failed');
     }
+});
+
+app.get('/total', async (req, res) => {
+    let _store = req.param('store');
+    let _brand = req.param('brand');
+    let _json = {
+           'gmv': '1823010188.23',
+           'base_achievement': '101.1%',
+           'target_phasing': '102.2%',
+           'base_target': '122223111',
+           'stretch': '18800000',
+           'evolution': '+101.2%',
+           'presell_gmv': '180203002.10',
+           'presell_convert': '100%'
+    }
+    res.send({
+        'error': 0,
+        'data': _json
+    });
+});
+
+app.get('/detail', async (req, res) => {
+    let _store = req.param('store');
+    let _brand = req.param('brand');
+    let _list = [
+        {
+            'store': 'Tmall',
+            'gmv': '20213001.23',
+            'base_achievement': '102.2%',
+            'stretch': '80.2%'
+        },
+        {
+            'store': 'JD',
+            'gmv': '10213001.23',
+            'base_achievement': '102.2%',
+            'stretch': '80.2%'
+        },
+        {
+            'store': 'Kaola',
+            'gmv': '8213001.23',
+            'base_achievement': '102.2%',
+            'stretch': '80.2%'
+        },
+        {
+            'store': 'Redbook',
+            'gmv': '6213001.23',
+            'base_achievement': '102.2%',
+            'stretch': '80.2%'
+        },
+        {
+            'store': 'Other',
+            'gmv': '4213001.23',
+            'base_achievement': '102.2%',
+            'stretch': '80.2%'
+        },
+    ];
+    res.send({
+        'error': 0,
+        'data': _list
+    })
 })
 
 app.listen(config.SERVER_PORT, () => console.log(`listening on port ${config.SERVER_PORT}!`));
